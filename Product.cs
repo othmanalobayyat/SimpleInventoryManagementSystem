822eaa6c476b644ea698f2c7b8907ede9f6f2289
OthmanAl-Obayyay <othmanalobayyat@gmail.com>
2024-09-28 18:53:28 +0300

    Solve the Problem

--- Product.cs
+++ Product.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace SimpleInventoryManagementSystem
 {
-    internal class Product
+    public class Product
     {
+        //Fields
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+
+        private int maxItemsInStock = 0;
+
+        private UnitType unitType;
+        private int amountInStock = 0;
+        private bool isBelowStockTreshold = false;
+
+        //Properties
+        public int Id
+        {
+            get { return id; }
+            set {  id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value.Length > 50 ? value[..50] : value;
+            }
+        }
+        public string? Description
+        {
+            get { return description; }
+            set
+            {
+                if (value == null)
+                    description = string.Empty;
+                else
+                {
+                    description = value.Length > 400 ? value[..400] : value;
+                }
+            }
+
+        }
+        public UnitType UnitType { get; set; }
+
+        public int AmountInStock { get; set; }
+        public bool IsBelowStockTreshold { get; set; }
+
+        //Constructors
+        public Product(int id,string name)
+        {
+            Id= id;
+            Name= name;
+        }
+
+        public Product(int id) : this(id, string.Empty)
+        { }
+
+        public Product (int id, string name, string? description, UnitType unitType, int maxAmountInStock)
+        {
+            Id=id;
+            Name= name;
+            Description= description;
+            UnitType= unitType;
+            this.maxItemsInStock = maxAmountInStock;
+        }
+
+        //Methods
+
+
+
+
+
+
     }
 }
